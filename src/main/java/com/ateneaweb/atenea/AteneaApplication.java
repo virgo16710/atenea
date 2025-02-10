3dbaa6719b2fde4aa515c9e213d047133ee0160b
virgo16710 <pablos15@hotmail.com>
2025-02-10 17:47:02 -0300

    main

---
+++ src/main/java/com/ateneaweb/atenea/AteneaApplication.java
@@ -0,0 +1,13 @@
+package com.ateneaweb.atenea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteneaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteneaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ateneaweb/atenea/AteneaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ateneaweb.atenea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AteneaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
